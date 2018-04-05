47998b77630180ceaaa50cc58a792a2f93307f7b
Taylor Smith <tgsmith61591@gmail.com>
2018-04-05 12:20:15 -0500

    Update build utils for skoot

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -4,16 +4,31 @@
 
 # License: 3-clause BSD
 
-set -e
+# 02/10/2018 remove due to Travis build issue 6307
+# set -e
+set +e  # because TRAVIS SUCKS
 
+# push coverage if necessary
 if [[ "$COVERAGE" == "true" ]]; then
+
     # Need to run coveralls from a git checkout, so we copy .coverage
     # from TEST_DIR where nosetests has been run
     cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
     cd $TRAVIS_BUILD_DIR
-    # Ignore coveralls failures as the coveralls server is not
+
+    # Ignore codecov failures as the codecov server is not
     # very reliable but we don't want travis to report a failure
     # in the github UI just because the coverage report failed to
     # be published.
-    coveralls || echo "Coveralls upload failed"
+    codecov || echo "codecov upload failed"
 fi
+
+# make sure we have twine in case we deploy
+pip install twine || "pip installing twine failed"
+
+# remove the .egg-info dir so Mac won't bomb on bdist_wheel cmd (absolute path in SOURCES.txt)
+rm -r skoot.egg-info/ || echo "No local .egg cache to remove"
+
+# make a dist folder if not there, then make sure permissions are sufficient
+mkdir -p dist
+chmod 777 dist
--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,71 +1,98 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
 
 set -e
 
+echo 'List files from cached directories'
+echo 'pip cache:'
+ls $HOME/.cache/pip
+
+# only do ccache if CACHEC is true. For now, this is FALSE on osx testing,
+# but might become true later (which is why we don't test for OS name instead)
+if [[ "$CACHEC" == true ]]; then
+    export CC=/usr/lib/ccache/gcc
+    export CXX=/usr/lib/ccache/g++
+    # Useful for debugging how ccache is used
+    # export CCACHE_LOGFILE=/tmp/ccache.log
+    # ~60M is used by .ccache when compiling from scratch at the time of writing
+    ccache --max-size 100M --show-stats
+fi
+
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead. if it's mac osx, there might not be a virtualenv
+# running, so deactivate would fail.
+deactivate || echo "No virtualenv or condaenv to deactivate"
+
 if [[ "$DISTRIB" == "conda" ]]; then
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
 
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    pushd .
-    cd
-    mkdir -p download
-    cd download
-    echo "Cached in $HOME/download :"
-    ls -l
-    echo
-    if [[ ! -f miniconda.sh ]]
-        then
-        if [[ "$PYTHON_VERSION" == "2.7" ]]; then
-            wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
-            -O miniconda.sh
-        fi
-        if [[ "$PYTHON_VERSION" == "3.5" ]]; then
-            wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-            -O miniconda.sh
-        fi
-    fi
-    chmod +x miniconda.sh && ./miniconda.sh -b
-    cd ..
-
-    # Set conda command relative to miniconda version.
-    if [[ "$PYTHON_VERSION" == "2.7" ]]; then
-        export PATH=/home/travis/miniconda/bin:$PATH
-    fi
-    if [[ "$PYTHON_VERSION" == "3.5" ]]; then
-        export PATH=/home/travis/miniconda3/bin:$PATH
+    # Install miniconda (if linux, use wget; if OS X, use curl)
+    # using the script we download
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+        MINICONDA_PATH=/home/travis/miniconda
+    else
+        curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh
+        MINICONDA_PATH=/Users/travis/miniconda
     fi
 
+    # append the path, update conda
+    chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
+    export PATH=$MINICONDA_PATH/bin:$PATH
     conda update --yes conda
-    popd
 
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    conda create -n testenv --yes python=$PYTHON_VERSION numpy scipy coverage cython
+    TO_INSTALL="python=$PYTHON_VERSION pip pytest pytest-cov \
+                numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
+                scikit-learn=$SCIKIT_LEARN_VERSION \
+                cython=$CYTHON_VERSION pandas=$PANDAS_VERSION"
 
+    if [[ "$INSTALL_MKL" == "true" ]]; then
+        TO_INSTALL="$TO_INSTALL mkl"
+    else
+        TO_INSTALL="$TO_INSTALL nomkl"
+    fi
+
+    conda create -n testenv --yes $TO_INSTALL
     source activate testenv
 
-    if [[ "$PYTHON_VERSION" == "2.7" ]]; then
-      conda install --yes -c dan_blanchard python-coveralls nose-cov;
-    fi
+    # determine what platform is running
+    python -c 'from distutils.util import get_platform; print(get_platform())'
 
-    pip install scikit-learn==$SCIKIT_LEARN_VERSION
-    pip install pandas # ==0.19.0
+# We haven't setup other distribution options yet
+else
+    echo "You've done screwed up your .travis.yml"
+    exit 10
+fi
 
-    if [[ "$WITH_MATPLOTLIB" == "true" ]]; then
-        pip install matplotlib;
-    fi
+# use PIP for installing coverage tools since we might not be using a conda dist
+if [[ "$COVERAGE" == "true" ]]; then
+    pip install coverage codecov
+fi
 
-    if [[ "$WITH_SEABORN" == "true" ]]; then
-        pip install seaborn;
-    fi
+if [[ "$TEST_DOCSTRINGS" == "true" ]]; then
+    pip install sphinx numpydoc  # numpydoc requires sphinx
+fi
 
-    pip install coveralls
-    pip install http://h2o-release.s3.amazonaws.com/h2o/rel-turchin/9/Python/h2o-3.8.2.9-py2.py3-none-any.whl
-    # Install nose-timer via pip
-    pip install nose-timer
+# now run the python setup. This implicitly builds all the C code with build_ext
+python setup.py develop
 
-    python setup.py develop;
+# Build pyramid in the install.sh script to collapse the verbose
+# build output in the travis output when it succeeds.
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import sklearn; print('sklearn %s' % sklearn.__version__)"
+python -c "\
+try:
+    import pandas
+    print('pandas %s' % pandas.__version__)
+except ImportError:
+    pass
+"
+
+# Only show the CCACHE stats if linux
+if [[ "$CACHEC" == true ]]; then
+    ccache --show-stats
 fi
