47a114984d0fd9f671579e928e8c3ef38b352038
Taylor Smith <tgsmith61591@gmail.com>
2018-06-14 09:47:55 -0500

    Back to doctest...

--- skoot/exploration/multivariate.py
+++ skoot/exploration/multivariate.py
@@ -60,8 +60,9 @@ def summarize(X):
     --------
     >>> import skoot
     >>> from skoot.datasets import load_iris_df
-    >>> skoot.summarize(load_iris_df())  # doctest:+SKIP
-                   sepal length (cm)  sepal width (cm)  petal length (cm)  \
+    >>> X = load_iris_df()
+    >>> skoot.summarize(X)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+                   sepal length (cm)  sepal width (cm)  petal length (cm) ...
     Mean                    5.843333          3.054000           3.758667
     Median                  5.800000          3.000000           4.350000
     Max                     7.900000          4.400000           6.900000
@@ -75,8 +76,8 @@ def summarize(X):
     Num Levels                   NaN               NaN                NaN
     Arity                        NaN               NaN                NaN
     Missing                 0.000000          0.000000           0.000000
-
-                   petal width (cm)    species
+    <BLANK_LINE>
+    ...            petal width (cm)    species
     Mean                   1.198667        NaN
     Median                 1.300000        NaN
     Max                    2.500000        NaN
