c3703f89c279e44a0383f04fd717d405672911e8
Taylor Smith <tgsmith61591@gmail.com>
2018-06-19 20:27:54 -0500

    Fix bug in example and add setup.py for datasets/

--- examples/preprocessing/ex_one_hot.py
+++ examples/preprocessing/ex_one_hot.py
@@ -17,6 +17,7 @@ print(__doc__)
 
 from skoot.datasets import load_adult_df
 from skoot.preprocessing import DummyEncoder
+from skoot.utils.dataframe import get_categorical_columns
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
@@ -24,19 +25,23 @@ import pandas as pd
 # load & split the data
 adult = load_adult_df(tgt_name="target")
 y = adult.pop("target")
+
+# we don't want this column
+adult.pop("education-num")
+
 X_train, X_test, y_train, y_test = train_test_split(adult, y, random_state=42,
                                                     test_size=0.2)
 
 # #############################################################################
 # Fit a dummy encoder
-obj_cols = X_train.select_dtypes(['object', 'int64']).columns
+obj_cols = get_categorical_columns(X_train)
 encoder = DummyEncoder(cols=obj_cols, handle_unknown='ignore')
 encoder.fit(X_train, y_train)
 
 # #############################################################################
 # Apply to the test set
 print("Test transformation:")
-print(encoder.transform(X_test))
+print(encoder.transform(X_test).head())
 
 # #############################################################################
 # Show we can work with levels we've never seen before
